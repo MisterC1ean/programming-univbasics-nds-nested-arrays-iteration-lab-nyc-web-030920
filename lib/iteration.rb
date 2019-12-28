fcf3dd69ba3fe8260710eb6aa7803bc150a5dfea
Robert Keller <robert_keller44@icloud.com>
2019-12-28 15:59:03 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -33,6 +33,8 @@ return greater_numbers_array
 end
 
 def total_even_pairs(src)
+  total_of_even_pairs = 0
+  
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
