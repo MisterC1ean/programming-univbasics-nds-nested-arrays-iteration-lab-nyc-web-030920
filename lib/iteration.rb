6139ded60f784fb624c69dbe6b18924519c6f7d9
Robert Keller <robert_keller44@icloud.com>
2019-12-28 15:41:11 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -25,6 +25,7 @@ while row_index < src.count do
     greater_numbers_array<< src[row_index][1]
 end
   row_index += 1
+  return greater_numbers_array
 end
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
